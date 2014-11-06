96529218221e99c824f05eab64517c13cb722c29
Wendy Nichols <wendlelyn@gmail.com>
2014-11-06 16:30:22 -0500

    fixing fixing fixing

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,71 +1,79 @@
 var arr = ["Tim","Wendy","Richard", "Chelsea", "Emory", "Victoria", "John", "Joanna","Max"];
-var wendy = {};
-//var root=this;
 
 
+(function() {
 
-    // _.first
-wendy.first = function(arr, funct) {
-    if (arr == null) return void 0;
-    if (funct == null) return arr[0];
-    if (funct < 0) return [];
-    return Array.prototype.slice.call(arr, 0, funct);
-};
-
-    // _.size
-wendy.size = function (list) {
-  return(list.length);
-};
-
-    // _.random
-wendy.random = function(min, max) {
-    if (max == null) {
-      max = min;
-      min = 0;
-    }
-    return min+Math.floor(Math.random() * (max - min + 1));
-};
-
-    // _.equal
-wendy.equal= function(a,b){
-if (a===b) return true;
-else{
-  return false;
-}
-};
-
-wendy.object = function( arr, function) {
-    if (arr == null) return {};
-    var result = {};
-    for (var i = 0, length = arr.length; i < length; i++) {
-      if (funct) {
-        result[arr[i]] = function[i];
-      }
-
-      else {
-        result[arr[i][0]] = arr[i][1];
-      }
-    }
-    return result;
-  };
+  var wendy = [];
+  var root=this;
+  root.wendy = wendy;
 
 
+        // _.first
+    wendy.first = function(arr, num) {
+      var firstVal = arr[0];
+        if (arr == null) return void 0;
+        if (num == null) return arr[0];
+        if (num < 0) return [];
+        return Array.prototype.slice.call(arr, 0, num);
+    };
 
-    // Class
-wendy.now = function(arr) {
-  return new Date().getTime();
-};
+        // _.size
+    wendy.size = function (list) {
+      return(list.length);
+    };
 
-wendy.map = function (arr, funct) {
-    if ( Array.isArray(arr) ) {
-        if (arr.length > 0) {
-          var res = [], i;
-          for (i = 0; i < arr.length; i++){
-          res[i] = funct(arr[i]);
+        // _.random
+    wendy.random = function(min, max) {
+        if (max == null) {
+          max = min;
+          min = 0;
         }
-        return res;
-      }
-      return [];
-    } else {
-    return console.log('This is not an array')
-  };
+        return min+Math.floor(Math.random() * (max - min + 1));
+    };
+
+        // _.equal
+    wendy.equal= function(a,b){
+    if (a===b) return true;
+    else{
+      return false;
+    }
+    };
+
+    wendy.object = function( arr, listCont) {
+        if (arr == null) {
+          return {};
+        };
+        var result = {};
+        for (var i = 0, length = arr.length; i < length; i++) {
+          if (listCont) {
+            result[arr[i]] = listCont[i];
+          }
+
+          else {
+            result[arr[i][0]] = listCont[i][1];
+          }
+        }
+        return result;
+      };
+
+
+
+    wendy.now = function(arr) {
+      return new Date().getTime();
+    };
+
+    wendy.map = function (arr, cont) {
+        if ( Array.isArray(arr) ) {
+            if (arr.length > 0) {
+              var res = [], i;
+              for (i = 0; i < arr.length; i++){
+              res[i] = cont(arr[i]);
+            }
+            return res;
+          }
+          return [];
+        } else {
+        return console.log('This is not an array')
+      };
+};
+}());
